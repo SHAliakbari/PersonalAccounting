1ca3233f8af6e06390fbb5daaab375ab8a88679b
Hassan Aliakbari <Amir.Aliakbari@hotmail.com>
2024-12-25 19:36:38 -0500

    -

--- PersonalAccounting.BlazorApp/Program.cs
+++ PersonalAccounting.BlazorApp/Program.cs
@@ -168,7 +168,7 @@ app.MigrateDbContext<ApplicationDbContext>(async (context, services) =>
     }
 
     var um = services.GetRequiredService<UserManager<ApplicationUser>>();
-    var admin = await um.FindByEmailAsync("shaliakbari@gmail.com");
+    var admin = await um.FindByEmailAsync("youremail@gmail.com");
     if (admin != null)
     {
         await um.AddToRoleAsync(admin, "admin");
