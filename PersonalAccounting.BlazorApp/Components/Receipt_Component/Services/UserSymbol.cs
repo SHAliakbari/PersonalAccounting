57c13f4302be5a534bab6c6f3cd86a2afe05dcc1
Hassan Aliakbari <Amir.Aliakbari@hotmail.com>
2025-01-25 13:13:24 -0500

    show share in add edit form too

---
+++ PersonalAccounting.BlazorApp/Components/Receipt_Component/Services/UserSymbol.cs
@@ -0,0 +1,21 @@
+﻿namespace PersonalAccounting.BlazorApp.Components.Receipt_Component.Services
+{
+    public class UserSymbol
+    {
+        public static readonly string[] Styles =
+        [
+            "bg-primary",
+            "bg-secondary",
+            "bg-success",
+            "bg-danger",
+            "bg-warning",
+            "bg-info",
+            "bg-light",
+            "bg-dark"
+        ];
+
+        public string UserName { get; set; }
+        public string Symbol { get; set; }
+        public string Style { get; set; }
+    }
+}
